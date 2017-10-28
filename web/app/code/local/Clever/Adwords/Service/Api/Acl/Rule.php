99e07abbe630776be99d10e91d480b4e00567729
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2017-10-28 15:49:23 +0200

    Add method to assign resource to role

--- web/app/code/local/Clever/Adwords/Service/Api/Acl/Rule.php
+++ web/app/code/local/Clever/Adwords/Service/Api/Acl/Rule.php
@@ -16,4 +16,28 @@ class Clever_Adwords_Service_Api_Acl_Rule
         return Mage::getModel('api2/acl_global_rule');
     }
 
+    /**
+     * @param $role
+     */
+    public function assignResources($role)
+    {
+        $_id = $role->getId();
+        $_resources = Clever_Adwords_Service_Settings::getResources();
+        $_rule = $this->getRule();
+        foreach ($_resources as $resourceId => $privileges) {
+            foreach ($privileges as $privilege => $allow) {
+                if (!$allow) {
+                    continue;
+                }
+
+                $_rule->setId(null)
+                    ->isObjectNew(true);
+
+                $_rule->setRoleId($_id)
+                    ->setResourceId($resourceId)
+                    ->setPrivilege($privilege)
+                    ->save();
+            }
+        }
+    }
 }

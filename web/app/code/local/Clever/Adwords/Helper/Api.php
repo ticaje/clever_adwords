ae7af1d50446249c12677539b3c454a08a794252
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2017-10-28 16:10:24 +0200

    Add API Helper reset resources method

--- web/app/code/local/Clever/Adwords/Helper/Api.php
+++ web/app/code/local/Clever/Adwords/Helper/Api.php
@@ -8,4 +8,21 @@
  */
 class Clever_Adwords_Helper_Api extends Mage_Core_Helper_Abstract
 {
+    /**
+     * @param Mage_Api2_Model_Acl_Global_Role $role
+     * @return false|Mage_Core_Model_Abstract
+     */
+    public function resetResourcesAclRules(Mage_Api2_Model_Acl_Global_Role $role)
+    {
+        $_id = $role->getId();
+        $_rule = Mage::getModel('api2/acl_global_rule');
+        if ($_id) {
+            $_collection = $_rule->getCollection()->addFilterByRoleId($_id);
+            /** @var $model Mage_Api2_Model_Acl_Global_Rule */
+            foreach ($_collection as $model) {
+                $model->delete();
+            }
+        }
+        return $_rule;
+    }
 }

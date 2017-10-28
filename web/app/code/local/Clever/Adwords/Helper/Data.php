65c78643bc2adbea01dec6ec1bd0fcd8b14e9778
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2017-10-28 15:18:39 +0200

    Add adwords module squeleton

---
+++ web/app/code/local/Clever/Adwords/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Helper Class
+ * @category    Clever
+ * @package     Clever_Adwords
+ * @author      Hector Luis Barrientos <ticaje@filetea.me>
+ */
+class Clever_Adwords_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}

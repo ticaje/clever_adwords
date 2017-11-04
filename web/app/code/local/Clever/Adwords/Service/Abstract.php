01aff325663494c5bdb790f28ab02ba6c89005d8
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2017-11-04 18:22:17 +0100

    Add service base abstract class

---
+++ web/app/code/local/Clever/Adwords/Service/Abstract.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Abstract Service Class
+ * @category    Clever
+ * @package     Clever_Adwords
+ * @author      Hector Luis Barrientos <ticaje@filetea.me>
+ */
+abstract class Clever_Adwords_Service_Abstract extends Mage_Core_Model_Abstract
+{
+
+}

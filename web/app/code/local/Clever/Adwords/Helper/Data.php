1c9dfa1ae991fc9dc90e8a62d56f8cbba972c553
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2017-11-04 09:42:54 +0100

    Add get websites list helper method

--- web/app/code/local/Clever/Adwords/Helper/Data.php
+++ web/app/code/local/Clever/Adwords/Helper/Data.php
@@ -8,5 +8,16 @@
  */
 class Clever_Adwords_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
+    public function getWebsitesList()
+    {
+        $_result = ['-1' => Mage::helper('clever_adwords')->__('Please Select')];
+        $_websites = Mage::getResourceModel( 'core/website_collection' );
+        foreach ($_websites as $index => $website){
+            $_values['label'] = $website->getName();
+            // I will just pick up the first one
+            $_values['value'] = current($website->getStoreIds());
+            $_result[$index] = $_values;
+        }
+        return $_result;
+    }
 }

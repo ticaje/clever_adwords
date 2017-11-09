c0331f9484e160e8d7133c2d13eb2ba1d2456c1e
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2017-11-09 13:15:49 +0100

    Wrap all interface logic in a single form

--- web/app/code/local/Clever/Adwords/Block/Adminhtml/Settings/Edit.php
+++ web/app/code/local/Clever/Adwords/Block/Adminhtml/Settings/Edit.php
@@ -8,17 +8,20 @@
  */
 class Clever_Adwords_Block_Adminhtml_Settings_Edit extends Mage_Adminhtml_Block_Widget_Form_Container
 {
+    protected $_helper;
+
     /**
      * Clever_Adwords_Block_Adminhtml_Configurations_Edit constructor.
      */
     public function __construct()
     {
         parent::__construct();
+        $this->_helper = Mage::helper('clever_adwords');
         $this->_objectId = 'id';
         $this->_blockGroup = 'clever_adwords';
         $this->_controller = 'adminhtml_settings';
         $this->_removeButton('reset');
-        if (Mage::helper('clever_adwords')->isInstalled()){
+        if ($this->_helper->isInstalled()) {
             $this->_removeButton('back');
             $this->_removeButton('save');
         }
@@ -29,8 +32,8 @@ class Clever_Adwords_Block_Adminhtml_Settings_Edit extends Mage_Adminhtml_Block_
      */
     public function getHeaderText()
     {
-        $_installed = Mage::helper('clever_adwords')->isInstalled();
+        $_installed = $this->_helper->isInstalled();
         $_header = !$_installed ? 'Install the Clever Adwords Application' : 'Clever Adwords Application Management';
-        return Mage::helper('clever_adwords')->__($_header);
+        return $this->_helper->__($_header);
     }
 }
--- web/app/code/local/Clever/Adwords/Block/Adminhtml/Settings/Edit/Form.php
+++ web/app/code/local/Clever/Adwords/Block/Adminhtml/Settings/Edit/Form.php
@@ -6,7 +6,7 @@
  * @package     Clever_Adwords
  * @author      Hector Luis Barrientos <ticaje@filetea.me>
  */
-class Clever_Adwords_Block_Adminhtml_Settings_Edit_Form extends Mage_Adminhtml_Block_Widget_Form
+class Clever_Adwords_Block_Adminhtml_Settings_Edit_Form extends Clever_Adwords_Override_Block_Widget_Form
 {
     /** @var  string */
     protected $_url_to_save;
@@ -26,15 +26,50 @@ class Clever_Adwords_Block_Adminhtml_Settings_Edit_Form extends Mage_Adminhtml_B
      */
     protected function _prepareForm()
     {
-        $form = new Varien_Data_Form(array(
+        $_form = new Varien_Data_Form(array(
                 'id' => 'edit_form',
                 'action' => $this->getUrl($this->_url_to_save, array('id' => $this->getRequest()->getParam('id'))),
                 'method' => 'post',
                 'enctype' => 'multipart/form-data'
             )
         );
-        $form->setUseContainer(true);
-        $this->setForm($form);
+
+        $_data = $this->register('settings_data');
+        $this->setForm($_form);
+
+        if ($this->_helper->isInstalled()) {
+            $_fieldset = $_form->addFieldset('iframe', array('legend' => $this->_helper->__('Dashborad')));
+
+            $_fieldset->addType('dashboard', 'Clever_Adwords_Block_Adminhtml_Iframe_Renderer');
+            $_fieldset->addField('dashboard', 'dashboard', array(
+                'name'      => 'dashboard',
+            ));
+
+        } else {
+            $_fieldset = $_form->addFieldset('install', array('legend' => $this->_helper->__("Store Info")));
+
+            $_websites = $this->_helper->getWebsitesList();
+            $_fieldset->addField('store', 'select', array(
+                'label' => $this->_helper->__("Select Website"),
+                'class' => 'required-entry',
+                'required' => true,
+                'name' => 'store',
+                'values' => $_websites,
+            ));
+
+            $_fieldset->addField('email', 'text', array(
+                'label' => $this->_helper->__("Store's Main Email"),
+                'class' => 'required-entry',
+                'required' => true,
+                'name' => 'email',
+            ));
+
+        }
+
+        $_form->setValues($_data);
+
+        $_form->setUseContainer(true);
+        $this->setForm($_form);
         return parent::_prepareForm();
     }
 

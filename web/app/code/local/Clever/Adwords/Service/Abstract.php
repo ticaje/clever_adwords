c69bbba84ab1f64826dffec7c449b80afcaf8656
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2017-11-19 19:19:05 +0100

    Cleanup base class

--- web/app/code/local/Clever/Adwords/Service/Abstract.php
+++ web/app/code/local/Clever/Adwords/Service/Abstract.php
@@ -8,22 +8,7 @@
  */
 abstract class Clever_Adwords_Service_Abstract extends Varien_Object
 {
-    const SHA_256 = 'sha256';
     protected $_store;
     protected $_email;
 
-    protected function getHashMacAlgorithm()
-    {
-        return self::SHA_256;
-    }
-
-    protected function getHashSecret()
-    {
-        return '4n7fdidvdrzvwe5hb0i4blohf4d8crc'; // Will be stored later on
-    }
-
-    protected function persistStoreHash()
-    {
-
-    }
 }

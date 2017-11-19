2955d4b7af26f2ccbfdd130e493321474cf1878d
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2017-11-19 19:02:23 +0100

    Add abstract service class's basic methods

--- web/app/code/local/Clever/Adwords/Service/Abstract.php
+++ web/app/code/local/Clever/Adwords/Service/Abstract.php
@@ -6,7 +6,24 @@
  * @package     Clever_Adwords
  * @author      Hector Luis Barrientos <ticaje@filetea.me>
  */
-abstract class Clever_Adwords_Service_Abstract extends Mage_Core_Model_Abstract
+abstract class Clever_Adwords_Service_Abstract extends Varien_Object
 {
+    const SHA_256 = 'sha256';
+    protected $_store;
+    protected $_email;
 
+    protected function getHashMacAlgorithm()
+    {
+        return self::SHA_256;
+    }
+
+    protected function getHashSecret()
+    {
+        return '4n7fdidvdrzvwe5hb0i4blohf4d8crc'; // Will be stored later on
+    }
+
+    protected function persistStoreHash()
+    {
+
+    }
 }

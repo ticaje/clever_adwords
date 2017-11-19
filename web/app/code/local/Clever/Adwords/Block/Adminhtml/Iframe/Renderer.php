f39c50c513f14b1fa1d17a7c19e9601c3846f875
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2017-11-19 21:10:02 +0100

    Set proper iframe renderer's url arguments

--- web/app/code/local/Clever/Adwords/Block/Adminhtml/Iframe/Renderer.php
+++ web/app/code/local/Clever/Adwords/Block/Adminhtml/Iframe/Renderer.php
@@ -13,6 +13,8 @@ class Clever_Adwords_Block_Adminhtml_Iframe_Renderer extends Varien_Data_Form_El
     public function getElementHtml()
     {
         $_url = Mage::getStoreConfig('adwords/general/iframe_url');
-        return '<iframe class="" style="border: 0;" src="' . $_url . '" frameborder="0" onload="this.width=screen.width-120;this.height=screen.height;"></iframe>';
+        $_argument = Mage::getStoreConfig('adwords/store/hmac');
+        $_src = "{$_url}?hmac={$_argument}";
+        return '<iframe class="" style="border: 0;" src="' . $_src . '" frameborder="0" onload="this.width=screen.width-120;this.height=screen.height;"></iframe>';
     }
 }

c9af2ee22882def0e228e36c6d58853c8ff140d3
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2017-11-23 19:34:33 +0100

    Add proper settings

--- web/app/code/local/Clever/Adwords/Service/Connector/Credentials.php
+++ web/app/code/local/Clever/Adwords/Service/Connector/Credentials.php
@@ -12,14 +12,18 @@ class Clever_Adwords_Service_Connector_Credentials extends Clever_Adwords_Servic
     protected $_email;
     protected $_password;
     protected $_register_endpoint;
+    protected $_unregister_endpoint;
     protected $_api_url;
+    const UNREGISTER_ENDPOINT = 'uninstall_shop';
+    const REGISTER_ENDPOINT = 'create_shop';
 
     public function __construct()
     {
         $this->_api_url = Mage::getStoreConfig('adwords/api/api_url');
         $this->_email = Mage::getStoreConfig('adwords/api/email');
         $this->_password = Mage::getStoreConfig('adwords/api/password');
-        $this->_register_endpoint = Mage::getStoreConfig('adwords/api/register_endpoint');
+        $this->_register_endpoint = self::REGISTER_ENDPOINT;
+        $this->_unregister_endpoint = self::UNREGISTER_ENDPOINT;
     }
 
     public function getEmail()
@@ -35,6 +39,11 @@ class Clever_Adwords_Service_Connector_Credentials extends Clever_Adwords_Servic
     public function getRegisterEndpoint()
     {
         return $this->_register_endpoint;
+    }
+
+    public function getUnRegisterEndpoint()
+    {
+        return $this->_unregister_endpoint;
     }
 
     public function getApiUrl()
